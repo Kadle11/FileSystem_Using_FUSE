10f680ade0d3b9e8060051c16ba7db4431cf6535
Vishal Rao <31560464+Kadle11@users.noreply.github.com>
2019-08-09 00:49:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Example Shell Script to Create Directories, Make a Directory called 'The_File_System' in the Parent Directory
+# ./TFS -f ../The_File_System
+# ./run.sh
+
 cd ../The_File_System
 mkdir Dir1
 cd Dir1
